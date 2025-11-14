40441851af6a1df5dc9a732d5c9220501f7d8e86
Admin <alhemister@gmail.com>
2025-11-14 20:46:39 +0300

    calcTest

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,18 @@
 plugins {
     id("java")
+    id("io.qameta.allure") version "2.11.2"
+}
+
+allure {
+    version.set("2.24.0")
+    adapter {                      // Настраиваем интеграцию с JUnit5
+        allureJavaVersion.set("2.24.0")
+        frameworks {
+            junit5 {
+                adapterVersion.set("2.24.0")
+            }
+        }
+    }
 }
 
 group = "org.example"
@@ -14,6 +27,9 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+// 📊 Allure
+    testImplementation("io.qameta.allure:allure-junit5:2.24.0")
+    testImplementation("io.qameta.allure:allure-java-commons:2.24.0")
 }
 
 tasks.test {
